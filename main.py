1436fbda26da55ef5ff2003914649d6bf80b1484
JFonS <joan.fonssanchez@gmail.com>
2015-11-10 09:31:31 +0100

    Fixed scrapper

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import telebot
+from telebot import util
+
+
+# Split the text each 3000 characters.
+# split_string returns a list with the splitted text.
+
 
 bot = telebot.TeleBot("179522397:AAFN1XumZ2TFfPkVOM14fO44NXMnttA8NFg")
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['test'])
+def handle_test(message):
+	large_text = open("color2emogi.txt", "r", encoding="utf-8").read()
+	splitted_text = util.split_string(large_text, 3000)
+	for text in splitted_text:
+		bot.send_message(message.chat.id, text)
+
 
 @bot.message_handler(content_types=['photo'])
 def handle_images(message):
-    bot.send_message(message.chat.id, "IMATGE")
+	bot.send_message(message.chat.id, "IMATGE")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+	bot.reply_to(message, message.text)
 
 bot.polling()
--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,12 @@
+# -*- coding: UTF-8 -*-
 from urllib import request
 import urllib.request
 from PIL import Image
 from PIL import ImageStat
+import codecs
 
-f = open("color2emogi.txt", "w")
+#f = open("color2emogi.txt", "w")
+f = codecs.open("color2emogi.txt", "w", "utf-8")
 
 url = "http://emojipedia.org/apple/"
 baseUrl = 'http://emojipedia.org'
@@ -28,11 +31,11 @@ while (urlStart != 9):
 	emojiReq  = request.Request(baseUrl + href, headers = myHeaders)
 	emojiPage = request.urlopen(emojiReq)
 	emojiPageStr = emojiPage.read().decode("utf-8")
-	print (baseUrl + href)
 
-	emojiStart = emojiPageStr.find("<span class=\"emoji\">") + 20
+	emojiStart = emojiPageStr.find("<h1><span class=\"emoji\">") + 24
 	emojiEnd = emojiPageStr.find("</span>", emojiStart)
 	emoji = emojiPageStr[emojiStart:emojiEnd]
+	print (str(i) + " -> " +  href[1:-1])
 
 	urlStart = pageStr.find("<img src=\"", urlEnd, end) + 10
 	urlEnd = pageStr.find("\"", urlStart, end)
@@ -40,7 +43,28 @@ while (urlStart != 9):
 
 	imgFile = request.urlopen(url)
 	img = Image.open(imgFile)
-	imgStats = ImageStat.Stat(img, img.split()[3])
-	f.write(emoji + " " + str(int(imgStats.mean[0])) + " " + str(int(imgStats.mean[1])) + " " + str(int(imgStats.mean[2])) + " // " + str(i) + " " + href[1:-1] + "\n")
-	print (emoji + " : " + str(imgStats.mean[0]) + "," + str(imgStats.mean[1]) + ","+ str(imgStats.mean[2])  )
+	pixels = img.getdata()
+
+	avg = (0,0,0)
+	nPixels = 0
+	#newData = []
+	for pixel in pixels:
+		nPixels = nPixels + 1
+		if pixel[3] < 10:
+			avg = (avg[0] + 255, avg[1] + 255, avg[2] + 255)
+			#newData.append((255,255,0))
+		else:
+			avg = (avg[0] + pixel[0], avg[1] + pixel[1], avg[2] + pixel[2])
+			#newData.append(pixel)
+	#img.putdata(newData)
+	#img.show()
+	avg = (float(avg[0])/float(nPixels),
+		   float(avg[1])/float(nPixels),
+		   float(avg[2])/float(nPixels))
+
+	print (avg[0], avg[1], avg[2])
+	#imgStats = ImageStat.Stat(img, img.split()[3])	
+
+	line = emoji + " " + str(int(avg[0])) + " " + str(int(avg[1])) + " " + str(int(avg[2])) + " // " + str(i) + " " + href[1:-1] + "\n"
+	f.write(line)
 	i = i + 1
